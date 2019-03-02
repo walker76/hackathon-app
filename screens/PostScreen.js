4fcd79f6034d585c5200afceba00dd38cb47440d
EBAKE <Evan_Baker@baylor.edu>
2019-03-02 12:24:55 -0600

    Begun creating job post screen

--- screens/PostScreen.js
+++ screens/PostScreen.js
@@ -3,18 +3,39 @@ import { ExpoConfigView } from '@expo/samples';
 import {
     ScrollView,
     StyleSheet,
-    Text,
     View,
   } from 'react-native';
+  import { Text, Input } from 'react-native-elements';
   
 export default class PostScreen extends React.Component {
   static navigationOptions = {
-    title: 'app.json',
+    title: 'Post Job',
   };
 
   render() {
     /* Go ahead and delete ExpoConfigView and replace it with your
      * content, we just wanted to give you a quick view of your config */
-    return <Text>This is the screen to post jobs</Text>;
+    return (
+    <View>
+      <Text h2>Post a New Job{"\n"}</Text>
+      <Input
+      placeholder='Job Title'
+      />
+      <Text>{"\n"}</Text> 
+      <Input
+      placeholder='Job Description'
+      />
+      <Text>{"\n"}</Text> 
+      <Input
+      placeholder=''
+      />
+        
+    </View>
+      
+      
+      
+    );
+
+
   }
 }
